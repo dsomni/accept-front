bd8eec760ebc2e8239491291777c9f89c74eb63c
Dmitry <pro100pro10010@gmail.com>
2021-10-24 23:01:54 +0300

    Init

---
+++ src/pages/index/Index.vue
@@ -0,0 +1,10 @@
+<template lang="pug" src="./template.pug"></template>
+<style lang="sass" src="./style.sass"></style>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
