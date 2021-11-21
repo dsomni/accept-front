49cb22aaf7f9b2481628ad481e62cd301c58fad0
Dmitry <pro100pro10010@gmail.com>
2021-11-21 11:43:23 +0300

    Add [ check viewMe before check authorization] routes index

--- src/router/index.js
+++ src/router/index.js
@@ -22,66 +22,17 @@ export default route(function ({store}) {
 
   Router.beforeEach(async (to, from, next) => {
 
-    // await api
-    // .get('/')
-    // .then((res) => {
-    //   console.log(res)
-    // })
-    // .catch((error) => {
-    //   console.log('Error')
-    //   if (error.response) {
-    //     console.log(error.response)
-    //   }
-    // });
-
-    // await api
-    // .get('api/user/1')
-    // .then((res) => {
-    //   console.log(res)
-    // })
-    // .catch((error) => {
-    //   console.log('Error')
-    //   if (error.response) {
-    //     console.log(error.response)
-    //   }
-    // });
-
-    // await api
-    // .get('api/user/11')
-    // .then((res) => {
-    //   console.log(res)
-    // })
-    // .catch((error) => {
-    //   console.log('Error')
-    //   if (error.response) {
-    //     console.log(error.response)
-    //   }
-    // });
-
-    // await api
-    // .put('api/user/11')
-    // .then((res) => {
-    //   console.log(res)
-    // })
-    // .catch((error) => {
-    //   console.log('Error')
-    //   if (error.response) {
-    //     console.log(error.response)
-    //   }
-    // });
-
-
     const isAuthenticated = store.getters['users/isAuthenticated'];
+    await store.dispatch('users/viewMe');
     if (isAuthenticated){
-      const response = await store.dispatch('users/viewMe');
-      await store.dispatch('users/refresh');
+     await store.dispatch('users/refresh');
     }
     if (to.matched.some(record => record.meta.requiresAuth)) {
       if (store.getters['users/isAuthenticated']) {
         next();
         return;
       }
-      // console.log(to)
+
       next({
         path: '/form/login',
         query: {
