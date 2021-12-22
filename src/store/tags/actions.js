a65f67519adc8e50c78dc5c7754dc58510db29b4
Dmitry <pro100pro10010@gmail.com>
2021-12-23 01:18:08 +0300

    Add [more tag axios] global

--- src/store/tags/actions.js
+++ src/store/tags/actions.js
@@ -69,3 +69,48 @@ export async function addTag({ dispatch }, tag) {
   return response;
 }
 
+export async function updateTag({ dispatch }, tag) {
+  let response = null;
+  await api
+    .put(`api/tag/${tag.spec}`, tag.title, tag.connectedTasks)
+    .then(async (res) => {
+      response = {
+        status: res.status,
+        statusText: res.statusText,
+      };
+    })
+    .catch((error) => {
+      if (error.response) {
+        response = {
+          status: error.response.status,
+          statusText: error.response.statusText,
+          detail: error.response.data.detail
+        };
+      }
+    });
+
+  return response;
+}
+
+export async function deleteTag({ dispatch }, tag) {
+  let response = null;
+  await api
+    .delete(`api/tag/${tag.spec}`)
+    .then(async (res) => {
+      response = {
+        status: res.status,
+        statusText: res.statusText,
+      };
+    })
+    .catch((error) => {
+      if (error.response) {
+        response = {
+          status: error.response.status,
+          statusText: error.response.statusText,
+          detail: error.response.data.detail
+        };
+      }
+    });
+
+  return response;
+}
