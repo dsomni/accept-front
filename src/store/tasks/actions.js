5e152f7afe23b76b96af549b90a8cbc506fe4ef0
Dmitry <pro100pro10010@gmail.com>
2022-01-12 17:21:53 +0300

    Add [getTaskBySpec request] store/actions

--- src/store/tasks/actions.js
+++ src/store/tasks/actions.js
@@ -26,7 +26,31 @@ export async function addTask({ dispatch }, task) {
 export async function getAllTasksToDisplay({ dispatch }) {
   let response = null;
   await api
-    .get('api/task/display-mode')
+    .get('api/task-display-mode')
+    .then(async (res) => {
+      response = {
+        status: res.status,
+        statusText: res.statusText,
+        data: res.data
+      };
+    })
+    .catch((error) => {
+      if (error.response) {
+        response = {
+          status: error.response.status,
+          statusText: error.response.statusText,
+          detail: error.response.data.detail
+        };
+      }
+    });
+
+  return response;
+}
+
+export async function getTaskBySpec({ dispatch }, spec) {
+  let response = null;
+  await api
+    .get(`api/task/${spec}`)
     .then(async (res) => {
       response = {
         status: res.status,
